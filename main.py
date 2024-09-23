9ea343c3b7f693c8e9a32e92fe1a1afa1f439c5a
mmilano55 <mmilanoomar12@gmail.com>
2024-09-23 02:51:50 +0100

    main.py

--- main.py
+++ main.py
@@ -267,7 +267,7 @@ def handle_games_callback(call):
 
 
 if __name__ == '__main__':
-    webhook_url = "https://bot20-yqao.onrender.com/"
+    webhook_url = "https://bot20-yqao.onrender.com/webhook"
     bot.remove_webhook()
     bot.set_webhook(url=webhook_url)
     app.run(host='0.0.0.0', port=3000)
